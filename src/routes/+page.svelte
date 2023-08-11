c3ba83cbe308f4d04ec335f96822d72a85773dd0
LazzariDev <lazzaridev@gmail.com>
2023-08-11 22:07:24 +0200

    Added Tailwind to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    console.log('Hello World!')
+</script>
+
+<h1 class="text-3xl font-bold underline text-green-600">
+    Hello world!!
+</h1>
