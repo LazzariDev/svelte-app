32ff5326874ea51037b5c9afb935fc1295f898f7
LazzariDev <lazzaridev@gmail.com>
2023-09-14 22:01:16 +0200

    Sub to stores

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,12 +1,19 @@
 <script>
 	import TaskList from "../../components/task-manager/TaskList.svelte";
-	import {taskListStore} from "../../stores/task"
+	import {taskListStore} from "../../stores/task";
 
 	const taskList = [
 		{id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
 		{id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
 		{id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
 	];
+
+	let _taskList
+
+	taskListStore.subscribe((value) => {
+		console.log('Subscribe called, '+ JSON.stringify(value));
+		_taskList = value;
+	})
 </script>
 
 <div class="p-10 h-full">
