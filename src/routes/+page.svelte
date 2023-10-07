37a4e8939f36a30c29d9446354c4db3458d093e3
LazzariDev <lazzaridev@gmail.com>
2023-10-07 19:52:24 +0200

    SSR / CSR / Prerendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 
+<script>
+    console.log('Calling Page!');
+    export let data;
+</script>
+
 <h1 class="text-3xl font-bold underline text-green-600">
     Hello world!!
+    <div>
+        {data.test1} - {data.test2}
+    </div>
 </h1>
