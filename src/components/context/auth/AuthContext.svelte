fe32ca52be431d7ba42d540559c4e72d5444f86e
LazzariDev <lazzaridev@gmail.com>
2023-10-08 18:39:17 +0200

    Auth context

---
+++ src/components/context/auth/AuthContext.svelte
@@ -0,0 +1,16 @@
+
+<script>
+	import { setContext } from "svelte";
+	import { writable } from "svelte/store";
+	import { key } from ".";
+
+    let isAuthenticated = writable(false);
+    let isLoading = writable(true);
+
+    setContext(key, {
+        isAuthenticated, isL
+    })
+
+</script>
+
+<slot />
