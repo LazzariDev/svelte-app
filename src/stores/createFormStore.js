63a5e30fcea9d17b12feb4f1117b5e9c296780e4
LazzariDev <lazzaridev@gmail.com>
2023-10-21 11:06:01 +0200

    Update Errors

--- src/stores/createFormStore.js
+++ src/stores/createFormStore.js
@@ -1,3 +1,4 @@
+import { error } from "@sveltejs/kit";
 import { writable } from "svelte/store"
 
 export function createFormStore(initialData) {
@@ -14,7 +15,13 @@ export function createFormStore(initialData) {
         const isValid = false;
 
         if (!isValid) {
-            errors.set({[element.name]: errorMessage})
+            /* errors.update((_errors) => {
+                _errors[element.name] = errorMessage;
+                return _errors;
+            }) */
+            // Upper code commented and botton line are the same
+            errors.update((_errors) => ({...errors, [element.name]: errorMessage}));
+
         } else {
             alert("No Errors!")
         }
