973a56266b637a9e9c19a50e606d0bc3d25f00bb
LazzariDev <lazzaridev@gmail.com>
2023-11-08 23:02:03 +0100

    Introduced snackbar concept

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
 	import UiContext from "@components/context/UI/UIContext.svelte";
     import "../app.css"
 	import AuthContext from "@components/context/auth/AuthContext.svelte";
+	import SnackbarContainer from "@components/snackbar/SnackbarContainer.svelte";
 </script>
 
 <UiContext>
     <AuthContext>
+        <SnackbarContainer />
         <slot />
     </AuthContext>
 </UiContext>
