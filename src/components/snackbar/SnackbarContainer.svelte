65a7f2daa189580448579345f723b5ba8a257c15
LazzariDev <lazzaridev@gmail.com>
2023-11-08 23:02:28 +0100

    Introduced snackbar concept

---
+++ src/components/snackbar/SnackbarContainer.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { getUiContext } from "@components/context/UI";
+    import SnackbarItem from "./SnackbarItem.svelte";
+
+    const { snackbars } = getUiContext(); 
+</script>
+  
+<div class="fixed z-50 top-0 right-0 p-4 w-ful md:max-w-xs">
+    <ul class="flex flex-col space-y-2">
+        {#each $snackbars as snackbar (snackbar) }
+            <SnackbarItem message={snackbar}/>
+        {/each}
+    </ul>
+</div>
