5a9ce720522f69c8aa53e946172b66d80c31458e
LazzariDev <lazzaridev@gmail.com>
2023-11-21 19:52:30 +0100

    Fetch glides

--- src/stores/createGlideStore.js
+++ src/stores/createGlideStore.js
@@ -1,12 +1,20 @@
+import { fetchGlides } from "@api/glides";
+import { onMount } from "svelte";
 import { writable } from "svelte/store"
 
 export function createGlideStore() {
     const glides = writable([]);
 
+    onMount(loadGlides);
+
+    async function loadGlides() {
+        const { glides } = await fetchGlides();
+        console.log(glides);
+    }
+
     function addGlide (glide) {
         glides.update(list => [glide, ...list])
     }
-
 
     return {
         glides: { subscribe: glides.subscribe },
